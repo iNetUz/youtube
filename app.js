796004d01645b23820b8e4e99d9cdd7ed0061831
iNetUz <116291789+iNetUz@users.noreply.github.com>
2024-05-30 14:46:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+function button() {
+    const element = document.getElementById("button");
+    element.className = "bgcolors";
+}
+function button1() {
+    const element = document.getElementById("button1");
+    element.className = "bgcolors";
+}
+function button2() {
+    const element = document.getElementById("button2");
+    element.className = "bgcolors";
+}
+function button3() {
+    const element = document.getElementById("button3");
+    element.className = "bgcolors";
+}
+function button4() {
+    const element = document.getElementById("button4");
+    element.className = "bgcolors";
+}
+function button5() {
+    const element = document.getElementById("button5");
+    element.className = "bgcolors";
+}
+function button6() {
+    const element = document.getElementById("button6");
+    element.className = "bgcolors";
+}
+function button7() {
+    const element = document.getElementById("button7");
+    element.className = "bgcolors";
+}
+function button8() {
+    const element = document.getElementById("button8");
+    element.className = "bgcolors";
+}
+function button9() {
+    const element = document.getElementById("button9");
+    element.className = "bgcolors";
+}
+function button10() {
+    const element = document.getElementById("button10");
+    element.className = "bgcolors";
+}
+function button11() {
+    const element = document.getElementById("button11");
+    element.className = "bgcolors";
+}
+function button12() {
+    const element = document.getElementById("button12");
+    element.className = "bgcolors";
+}
+function button13() {
+    const element = document.getElementById("button13");
+    element.className = "bgcolors";
+}
+function button14() {
+    const element = document.getElementById("button14");
+    element.className = "bgcolors";
+}
+function button15() {
+    const element = document.getElementById("button15");
+    element.className = "bgcolors";
+}
+function button16() {
+    const element = document.getElementById("button16");
+    element.className = "bgcolors";
+}
+function button17() {
+    const element = document.getElementById("button17");
+    element.className = "bgcolors";
+}
+function button18() {
+    const element = document.getElementById("button18");
+    element.className = "bgcolors";
+}
+function button19() {
+    const element = document.getElementById("button19");
+    element.className = "bgcolors";
+}
+function button20() {
+    const element = document.getElementById("button20");
+    element.className = "bgcolors";
+}
+function button21() {
+    const element = document.getElementById("button21");
+    element.className = "bgcolors";
+}
+function button22() {
+    const element = document.getElementById("button22");
+    element.className = "bgcolors";
+}
+function close() {
+    const element = document.getElementById("button22");
+    element.className = "bgcolor";
+}
